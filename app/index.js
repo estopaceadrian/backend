23266888cdf14d640e099bb405e3d106c044008d
Adrian Estopace <estopaceadrian@gmail.com>
2022-09-18 13:10:31 +0800

    added cors

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const GenerationEngine = require('./generation/engine');
+const cors = require('cors');
 
+const GenerationEngine = require('./generation/engine');
 const dragonRouter = require('./api/dragon.js');
 const generationRouter = require('./api/generation');
 
@@ -8,6 +9,8 @@ const app = express();
 const engine = new GenerationEngine();
 
 app.locals.engine = engine;
+
+app.use(cors({ origin: 'http://localhost:1234' }));
 
 app.use('/dragon', dragonRouter);
 app.use('/generation', generationRouter);
